5be460d65df350b2f27343b15f0522914f3a55c9
Alexandre Pérou <64700694+nigelnokushimodo@users.noreply.github.com>
2023-02-17 11:49:16 +0100

    debut de scéance

--- src/main/java/com.isep.tp2/Main.java
+++ src/main/java/com.isep.tp2/Main.java
@@ -1,4 +1,26 @@
 package com.isep.tp2;
 
 public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        boolean result = isBinarySumInArray(new int[]{1, 2, 4, 5}, 3);
+        System.out.println("Le résultat est " + result);
+    }
+
+    public static boolean isBinarySumInArray(int[] tableau, int val) {
+        int n = tableau.length;
+
+        for(int i = 0; i < n - 1; ++i) {
+            for(int j = i + 1; j < n; ++j) {
+                int somme = tableau[i] + tableau[j];
+                if (somme == val) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
